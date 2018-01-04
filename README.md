fb240d7785ea05e8a0b02c74f2eee33bac5af4cc
Salvatore Eckel <salvaracer@gmx.de>
2018-01-04 22:15:29 +0100

    [INIT] Initial upload to git. Version 2.2.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# EXT:t3content_bootstrap4 #
+
+This extension provides flux content elements  for the t3cms theme bootstrap4
+
+### General Information ###
+
+* t3content_bootstrap4
+* v2.2.0
+
+### How do I get set up? ###
+
+* Install the extension from TER
+* Make sure EXT:t3themes_bootstrap4 is installed
+
+* Have fun with your new content elements.
+
+### Contribution guidelines ###
+
+* We are happy, of you contribute with us!
+* Merge requests are wished,
+* PSR-2 guidlines
+
+### Who do I talk to? ###
+
+* Salvatore Eckel
+* salvaracer@gmx.de
---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+# Extension Manager/Repository config file for ext: "t3content_bootstrap4"
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 Content - Bootstrap4',
+    'description' => 'This extension provides flux content elements  for the t3cms theme bootstrap4',
+    'category' => 'templates',
+    'author' => 'Salvatore Eckel',
+    'author_email' => 'salvaracer@gmx.de',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '2.2.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.18-8.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
